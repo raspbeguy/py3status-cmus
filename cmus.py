c08fd289462fb8b0e2b364f5b7d75698690b17c2
raspbeguy <raspbeguy@hashtagueule.fr>
2016-11-19 00:28:30 +0100

    Added configuration variables, click and color support, used py3status project conventions

--- cmus.py
+++ cmus.py
@@ -1,8 +1,47 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Display information about the current song in cmus.
+
+Toggle play/pause by click.
+
+Configuration parameters:
+    format: see placeholders below
+        (default '{state} [{artist} - ][{title}]')
+    format_none: define output if no cmus is running
+        (default 'no player running')
+    state_pause: text for placeholder {state} when song is paused (default '▮')
+    state_play: text for placeholder {state} when song is playing (default '▶')
+    state_stop: text for placeholder {state} when song is stopped (default '◾')
+    state_unknown: text for placeholder {state} when song is in unknown status (defaut '?')
+
+Format of status string placeholders:
+    {album} album name
+    {artist} artiste name
+    {state} playback status of the player
+    {title} name of the song
+
+i3status.conf example:
+
+```
+cmus {
+    format = "{state} [[{artist} - {title}]|[{title}]]"
+    format_none = "no player"
+}
+```
+
+@author protosphere, raspbeguy
+"""
+
 import subprocess
 
 class Py3status:
     cache_timeout = 0
+    format = '{state} [{artist} - ][{title}]'
+    format_none = "no player"
+    state_pause = u'▮'
+    state_play = u'▶'
+    state_stop = u'◾'
+    state_unknown = u'?'
 
     def _get_cmus_info(self):
         cmus_remote_pipe = subprocess.Popen(["cmus-remote", "-Q"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
@@ -25,11 +64,21 @@ class Py3status:
     
         return cmus_info
 
-    def cmus(self, i3s_output_list, i3s_config):
-        response = {}
-        cmus_info = self._get_cmus_info();
-
+    def _get_text(self, cmus_info):
         if "status" in cmus_info:
+            if cmus_info["status"] == "playing":
+                color = self.py3.COLOR_PLAYING or self.py3.COLOR_GOOD
+                state_symbol = self.state_play
+            elif cmus_info["status"] == "paused":
+                color = self.py3.COLOR_PAUSED or self.py3.COLOR_DEGRADED
+                state_symbol = self.state_pause
+            elif cmus_info["status"] == "stopped":
+                color = self.py3.COLOR_STOPPED or self.py3.COLOR_BAD
+                state_symbol = self.state_stop
+            else:
+                color = self.py3.COLOR_BAD
+                state_symbol = self.state_unknown
+
             if "artist" in cmus_info["tags"]:
                 now_playing_artist = cmus_info["tags"]["artist"]
             else:
@@ -40,18 +89,35 @@ class Py3status:
             else:
                 now_playing_title = "Unknown"
 
-            response["full_text"] = "♫ : " + now_playing_artist + " - " + now_playing_title
-        else:
-            response["full_text"] = ""
+
+        placeholders = {
+            'state': state_symbol,
+            'album': cmus_info["tags"]["album"] if "album" in cmus_info["tags"] else "",
+            'artist': cmus_info["tags"]["artist"] if "artist" in cmus_info["tags"] else "",
+            'title': cmus_info["tags"]["title"] if "title" in cmus_info["tags"] else "",
+        }
+
+        return (placeholders, color)
+
+    def cmus(self, i3s_output_list, i3s_config):
+        cmus_info = self._get_cmus_info()
+        (text, color) = self._get_text(cmus_info)
+        full_text = self.py3.safe_format(self.format,text)
+        response = {
+            'color': color,
+            'full_text': full_text,
+        }
 
         return response
+
+    def on_click(self, event):
+        button = event['button']
+        if button == 1:
+            subprocess.call(["cmus-remote", "--pause"])
 
 if __name__ == "__main__":
     """
     Test this module by calling it directly.
     """
-    from time import sleep
-    x = Py3status()
-    while True:
-        print(x.cmus([], {}))
-        sleep(1)
+    from py3status.module_test import module_test
+    module_test(Py3status) 
